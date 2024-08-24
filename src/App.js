7db17e21713d4c974ff56f4cace504acb0ecaf1a
pushti <pushti.soni@rapidops.co>
2024-08-25 00:15:41 +0530

    final complete code of assignment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
     BrowserRouter as Router,
     Routes,
@@ -8,47 +8,50 @@ import {
 import { AuthProvider } from "./contexts/AuthContext";
 import { DishProvider } from "./contexts/DishContext";
 import PrivateRoute from "./components/PrivateRoute";
-import Login from "./components/Login";
-import Navigation from "./components/Navigation";
-import RankDishes from "./components/RankDishes";
-import TopRankedDishes from "./components/TopRankedDishes";
-import AdminPanel from "./components/AdminPanel";
-import './App.css'
+import './App.css';
+
+const Login = lazy(() => import("./components/Login"));
+const Navigation = lazy(() => import("./components/Navigation"));
+const RankDishes = lazy(() => import("./components/RankDishes"));
+const TopRankedDishes = lazy(() => import("./components/TopRankedDishes"));
+const AdminPanel = lazy(() => import("./components/AdminPanel"));
 
 const App = () => {
     return (
         <Router>
             <AuthProvider>
                 <DishProvider>
-                    <Navigation />
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route
-                            path="/"
-                            element={
-                                <PrivateRoute>
-                                    <RankDishes />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/top"
-                            element={
-                                <PrivateRoute>
-                                    <TopRankedDishes />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/admin"
-                            element={
-                                <PrivateRoute adminOnly={true}>
-                                    <AdminPanel />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </Routes>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Navigation />
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route
+                                path="/"
+                                element={
+                                    <PrivateRoute>
+                                        <RankDishes />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path="/top"
+                                element={
+                                    <PrivateRoute>
+                                        <TopRankedDishes />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route
+                                path="/admin"
+                                element={
+                                    <PrivateRoute adminOnly={true}>
+                                        <AdminPanel />
+                                    </PrivateRoute>
+                                }
+                            />
+                            <Route path="*" element={<Navigate to="/" />} />
+                        </Routes>
+                    </Suspense>
                 </DishProvider>
             </AuthProvider>
         </Router>
